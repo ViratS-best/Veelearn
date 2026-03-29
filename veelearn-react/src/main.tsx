b6d3ba262a9695e34b7c6b353b66e2aac72cfeb4
ViratS-best <asterloop@outlook.com>
2026-03-29 14:33:06 -0400

    Fix: Router basename to root

--- veelearn-react/src/main.tsx
+++ veelearn-react/src/main.tsx
@@ -18,7 +18,7 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Veelearn">
+    <BrowserRouter basename="/">
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <App />
